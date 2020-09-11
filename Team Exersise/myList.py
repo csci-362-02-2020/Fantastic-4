0e636afeb3934123423bfe40817ccd551d4f3ee5
Clare Clever <clevercc@g.cofc.edu>
2020-09-11 19:11:53 -0400

    Made the HTML file display in a table

--- Team Exersise/myList.py
+++ Team Exersise/myList.py
@@ -3,6 +3,12 @@ import webbrowser
 
 # Open the file
 fileOut = open("outFile.html", "w")
+StyleFileIn = open("style.html", "r")
+
+# Write the style to the html file (This style was found here... https://www.w3schools.com/html/tryit.asp?filename=tryhtml_table_intro)
+for line in StyleFileIn:
+    fileOut.write(line)
+fileOut.write("\n\n")
 
 new = 2 # open in a new tab, if possible
 
@@ -28,17 +34,29 @@ for item in osDIR:
 print()
 print("Constructing the html file...")
 
+# Html for the table
+fileOut.write("<table>\n\n")
+
 # Print the folders to the html file
-fileOut.write("<strong>Folders in current directory:</strong>\n<br />\n\n")
+fileOut.write("<tr><th><strong>Folders in current directory:</strong></tr></th>\n\n")
 
 for item in folderList:
-    fileOut.write("<h>" + item +"</h>\n<br />\n\n<br />\n\n")
+    fileOut.write("<tr><td>" + item +"</td></tr>\n\n")
+
+# Html for the table
+fileOut.write("</table>\n\n<br />\n\n")
+
+# Html for the table
+fileOut.write("<table>\n\n")
 
 # Print the files to the html file
-fileOut.write("<strong>Files in current directory:</strong>\n<br />\n\n")
+fileOut.write("<tr><th><strong>Files in current directory:</strong></tr></th>\n\n")
 
 for item in fileList:
-    fileOut.write("<h>" + item +"</h>\n<br />\n\n")
+    fileOut.write("<tr><td>" + item +"</td></tr>\n\n")
+
+# Html for the table
+fileOut.write("</table>\n\n<br />\n\n")
 
 # Close the file
 fileOut.close()
